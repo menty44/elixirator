6f01af5e9915961423863148b2f6d604a03adeae
amal <amalsultan999@gmail.com>
2021-10-01 19:18:32 +0500

    Implements fuel calculation algorithm

--- lib/elixirator.ex
+++ lib/elixirator.ex
@@ -2,17 +2,4 @@ defmodule Elixirator do
   @moduledoc """
   Documentation for `Elixirator`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixirator.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/elixirator_test.exs
+++ test/elixirator_test.exs
@@ -1,8 +1,4 @@
 defmodule ElixiratorTest do
   use ExUnit.Case
   doctest Elixirator
-
-  test "greets the world" do
-    assert Elixirator.hello() == :world
-  end
 end
